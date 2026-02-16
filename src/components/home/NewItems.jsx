44493021bfd00c2eb6e3e858c1f291ae53817dc6
David <davidsaleh03@gmail.com>
2026-02-15 22:12:28 -0800

    author

--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -54,7 +54,7 @@ const NewItems = () => {
                   <div className="nft__item">
                     <div className="author_list_pp">
                       <Link
-                        to="/author"
+                        to={"/author"}
                         data-bs-toggle="tooltip"
                         data-bs-placement="top"
                         title="Creator: Monica Lucas"
